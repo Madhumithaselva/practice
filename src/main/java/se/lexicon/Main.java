5cfcad90f51d20ac179757c38ab596054da5f7a0
Madhu <madhu8129@gmail.com>
2023-11-16 10:49:42 +0100

    First commit

---
+++ src/main/java/se/lexicon/Main.java
@@ -0,0 +1,12 @@
+package se.lexicon;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String name="Madhumitha";
+        String wavingHand = "\uD83D\uDC4B"; //initialising a string with waving hand emoji unicode
+        // Print a message with the waving hand emoji
+        System.out.println("Hello "+ name +"!"+ wavingHand);
+        System.out.println(heart);
+    }
+}
